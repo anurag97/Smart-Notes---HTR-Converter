2bbe1accd65481908db8f7661124b8a827f19ae7
Jatin Khachane <54959993+jatink12@users.noreply.github.com>
2020-04-20 19:14:04 +0530

    Update runner.py

--- runner.py
+++ runner.py
@@ -3,14 +3,9 @@ import matplotlib.pyplot as plt
 import cv2
 
 scanner = scannerClass()
-image2 = scanner.getTransformedImage('sample5.jpg')
 
-print(image2.shape)
+#image path as parameter
+image2 = scanner.getTransformedImage('')
 
-ret, thresh1 = cv2.threshold(image2, 0, 255, cv2.THRESH_BINARY) 
-
-plt.imshow(thresh1,cmap='gray')
+plt.imshow(image2,cmap='gray')
 plt.show()
-
-
-cv2.imwrite('out.jpg',thresh1)
