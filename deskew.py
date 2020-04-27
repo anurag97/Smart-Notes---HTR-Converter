e6fad45dc776eed7809e3a6a3ac6633af4dc533f
Manish Chetwani <chetwani.manish03@gmail.com>
2020-04-27 16:31:59 +0530

    Update deskew.py

--- deskew.py
+++ deskew.py
@@ -4,15 +4,15 @@
 # Imports Req - from scipy.ndimage import interpolation       #
 
 def moments(image):
-    c0,c1 = np.mgrid[:image.shape[0],:image.shape[1]] # A trick in numPy to create a mesh grid
+    c0,c1 = np.mgrid[:image.shape[0],:image.shape[1]]
     totalImage = np.sum(image) #sum of pixels
     m0 = np.sum(c0*image)/totalImage #mu_x
     m1 = np.sum(c1*image)/totalImage #mu_y
     m00 = np.sum((c0-m0)**2*image)/totalImage #var(x)
     m11 = np.sum((c1-m1)**2*image)/totalImage #var(y)
     m01 = np.sum((c0-m0)*(c1-m1)*image)/totalImage #covariance(x,y)
-    mu_vector = np.array([m0,m1]) # Notice that these are \mu_x, \mu_y respectively
-    covariance_matrix = np.array([[m00,m01],[m01,m11]]) # Do you see a similarity between the covariance matrix
+    mu_vector = np.array([m0,m1]) 
+    covariance_matrix = np.array([[m00,m01],[m01,m11]])
     return mu_vector, covariance_matrix
 
 def deskew(image):
